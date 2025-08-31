1c29e58f01cd6eb5032105d1bc37953caafced64
harsith1410 <harsith1410@gmail.com>
2025-08-31 17:47:34 +0530

    Homepage basic linking and styling done

--- src/main/java/com/portfolio/controller/page_controller.java
+++ src/main/java/com/portfolio/controller/page_controller.java
@@ -2,28 +2,31 @@ package com.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@RequestMapping("/ironman112")
 public class page_controller {
 
-    @GetMapping("/home")
+    @GetMapping("/ironman112/home")
     public String ironman112() {
         return "home";
     }
+
+
     @GetMapping("/about")
-    public String about() {
+    public String showAboutPage() {
         return "about";
     }
+
+
     @GetMapping("/projects")
-    public String projects() {
-        return "Projects";
+    public String showProjectsPage() {
+        return "projects";
     }
+
+
     @GetMapping("/skills")
-    public String skills() {
+    public String showContactPage() {
         return "Skills";
     }
-
-
 }
